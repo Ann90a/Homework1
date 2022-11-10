df0f05d4574e7f266d031e744298e4fda2dcf56c
Ann90a <abrosimova222@yandex.ru>
2022-11-10 13:32:03 +0300

    задание 2

--- src/Main.java
+++ src/Main.java
@@ -4,7 +4,14 @@ public class Main {
 
    //задание 1
     public static void main(String[] args) {
-        for (int i = 1; i <= 10; i++ ) {
+        for (int i = 1; i <= 10; i++) {
+            System.out.println("Итерация цикла " + i);
+        }
+
+
+        //задание 2
+
+        for (int i = 10; i >= 1; i--) {
             System.out.println("Итерация цикла " + i);
         }
 
