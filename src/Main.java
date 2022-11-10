3cb25de729c66c35c5e4e6c97d1276054f9af387
Ann90a <abrosimova222@yandex.ru>
2022-11-10 14:38:29 +0300

    задание 3

--- src/Main.java
+++ src/Main.java
@@ -16,6 +16,16 @@ public class Main {
         }
 
 
+        //задание 3
+
+        for (int i = 0; i <= 17; i++)
+            if (i % 2 == 0) {
+            System.out.println("Все четные числа " + i);
+        }
+
+
+
+
 
     }
 }
